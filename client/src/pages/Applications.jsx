b11acedfc8abab5b908bf35a7c6b96d82acee370
Roshan-Sam <roshansamchirathalackal9898@gmail.com>
2025-03-21 19:20:55 +0530

    completed ui section of recruiter dashboard, manage jobs, view applications, add job

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -34,37 +34,39 @@ const Applications = () => {
                     }
                 </div>
                 <h2 className='text-xl font-semibold mb-4'>Job Applied</h2>
-                <table className='min-w-full bg-white border rounded-lg'>
-                    <thead>
-                        <tr>
-                            <th className='py-3 px-4 border-b text-left'>Company</th>
-                            <th className='py-3 px-4 border-b text-left'>Job Title</th>
-                            <th className='py-3 px-4 border-b text-left max-sm:hidden'>Location</th>
-                            <th className='py-3 px-4 border-b text-left max-sm:hidden'>Date</th>
-                            <th className='py-3 px-4 border-b text-left'>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {jobsApplied.map((job, index) => (
-                            true ? (
-                                <tr key={index}>
-                                    <td className='py-3 px-4 flex items-center gap-2 border-b'>
-                                        <img className='w-8 h-8' src={job.logo} alt="" />
-                                        {job.company}
-                                    </td>
-                                    <td className='py-2 px-4 border-b'>{job.title}</td>
-                                    <td className='py-2 px-4 border-b max-sm:hidden'>{job.location}</td>
-                                    <td className='py-2 px-4 border-b max-sm:hidden'>{moment(job.date).format('ll')}</td>
-                                    <td className='py-2 px-4 border-b'>
-                                        <span className={`${job.status === 'Accepted' ? 'bg-green-100' : job.status === 'Rejected' ? 'bg-red-100' : 'bg-blue-100'} px-4 py-1.5 rounded`}>
-                                            {job.status}
-                                        </span>
-                                    </td>
-                                </tr>
-                            ) : (null)
-                        ))}
-                    </tbody>
-                </table>
+                <div className='overflow-x-scroll'>
+                    <table className='min-w-full bg-white border rounded-lg'>
+                        <thead>
+                            <tr>
+                                <th className='py-3 px-4 border-b text-left'>Company</th>
+                                <th className='py-3 px-4 border-b text-left'>Job Title</th>
+                                <th className='py-3 px-4 border-b text-left max-sm:hidden'>Location</th>
+                                <th className='py-3 px-4 border-b text-left max-sm:hidden'>Date</th>
+                                <th className='py-3 px-4 border-b text-left'>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {jobsApplied.map((job, index) => (
+                                true ? (
+                                    <tr key={index}>
+                                        <td className='py-3 px-4 flex items-center gap-2 border-b'>
+                                            <img className='w-8 h-8' src={job.logo} alt="" />
+                                            {job.company}
+                                        </td>
+                                        <td className='py-2 px-4 border-b'>{job.title}</td>
+                                        <td className='py-2 px-4 border-b max-sm:hidden'>{job.location}</td>
+                                        <td className='py-2 px-4 border-b max-sm:hidden'>{moment(job.date).format('ll')}</td>
+                                        <td className='py-2 px-4 border-b'>
+                                            <span className={`${job.status === 'Accepted' ? 'bg-green-100' : job.status === 'Rejected' ? 'bg-red-100' : 'bg-blue-100'} px-4 py-1.5 rounded`}>
+                                                {job.status}
+                                            </span>
+                                        </td>
+                                    </tr>
+                                ) : (null)
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
             <Footer />
         </>
